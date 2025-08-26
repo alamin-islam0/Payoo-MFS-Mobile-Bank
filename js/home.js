20c1abec57b503e00b5ee0de4e6d072538ced74a
Alamin Islam <alamin648890@gmail.com>
2025-08-26 12:32:45 +0600

    bug fixed

--- js/home.js
+++ js/home.js
@@ -1,15 +1,26 @@
+    const validPin = 1234;
+
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
     e.preventDefault ();
-
     const bankName = document.getElementById("bank-name").value;
 
     const bankAccountNumber = document.getElementById("bank-acc-number").value;
 
+    if (bankAccountNumber.length < 11) {
+        alert('Please provide valid account number');
+        return;
+    }
+
     const addAmount = parseInt(document.getElementById("add-amount").value);
 
-    const pin = document.getElementById("add-pin").value;
+    const pin = parseInt(document.getElementById("add-pin").value);
+
+    if (pin !== validPin) {
+        alert('Please provide valid pin number');
+        return;
+    }
 
     const availableBalance = parseInt(
       document.getElementById("available-balance").innerText);
@@ -18,3 +29,15 @@ document
 
     document.getElementById("available-balance").innerText = totalNewAvailableBalance;
   });
+
+//   Toggling feature
+
+document.getElementById('addmoney-btn').addEventListener('click', function () {
+    document.getElementById('cashout-parent').style.display = "none";
+    document.getElementById('add-money-parent').style.display = "block";
+});
+
+document.getElementById('cashout-btn').addEventListener('click', function () {
+    document.getElementById('cashout-parent').style.display = "block";
+    document.getElementById('add-money-parent').style.display = "none";
+});
